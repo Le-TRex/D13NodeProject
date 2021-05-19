6562bc4f328525017907a256e52ab99d2fa28907
Le-TRex <clarembo@gmail.com>
2021-05-19 15:45:24 +0200

    :truck: :recycle: refactor routes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const expressLayouts = require("express-ejs-layouts");
 const { urlencoded } = require("express");
-const Project = require("./models/project");
+const projectRoutes = require("./routes/projectRoutes")
 
 const app = express();
 
@@ -16,60 +16,8 @@ app.use(expressLayouts);
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 
-/**
- * GET
- */
 app.get("/", (request, response) => {
-    response.redirect("projects");
+  response.redirect("projects");
 });
 
-app.get("/projects", (request, response) => {
-  Project.find()
-    .then(projects => response.render("projects", {projects: projects}))
-    .catch(error => console.log(error));
-});
-
-/**
- * CREATE
- */
-app.get("/projects/create", (request, response) => {
-    response.render("projects/create");
-})
-
-app.post("/projects/create", (request, response) => {
-  Project.create(request.body)
-    .then(() => response.redirect("/projects"))
-    .then(error => console.log(error));
-})
-
-/**
- * UPDATE
- */
-app.get("/projects/update/:id", (request, response) => {
-  const id = request.params.id;
-  Project.findById(id)
-    .then(project => response.render("projects/update", { project: project }))
-    .catch(error => console.log(error));
-})
-
-app.post("/projects/update", (request, response) => {
-  const project = request.body;
-  Project.findByIdAndUpdate(project._id, { name: project.name, description: project.description })
-    .then(() => response.redirect("/projects"))
-    .catch(error => console.log(error));
-})
-
-/**
- * DELETE
- */
-
-app.get("/projects/delete/:id", (request, response) => {
-  const id = request.params.id;
-  response.render("projects/delete", { _id: id }); //pas de / devant les chemins lors des renders
-})
-
-app.post("/projects/delete", (request, response) => {
-  Project.findByIdAndDelete(request.body._id)
-    .then(() => response.redirect("/projects"))
-    .catch(error => console.log(error));
-})
+app.use("/projects", projectRoutes); //"/projects" => alias pour le début de chaque route
---
+++ routes/projectRoutes.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const Project = require("../models/project");
+const projectController = require("../controllers/projectController");
+
+/**
+ * GET
+ */
+router.get("/", projectController.all);
+
+/**
+* CREATE
+*/
+router.get("/create", projectController.createGet);
+router.post("/create", projectController.createPost);
+
+/**
+* UPDATE
+*/
+router.get("/update/:id", projectController.updateGet);
+router.post("/update", projectController.updatePost);
+
+/**
+* DELETE
+*/
+router.get("/delete/:id", projectController.deleteGet);
+router.post("/delete", projectController.deletePost);
+
+module.exports = router;
