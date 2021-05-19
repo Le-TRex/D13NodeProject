a7e2f59ff23e9fc697f7d9a6a548f81f3dce1378
Le-TRex <clarembo@gmail.com>
2021-05-19 09:12:45 +0200

    :construction: build

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("project init")
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const dbUri = "mongodb://localhost:27017/projectManagement";
+mongoose.connect(dbUri, {useUnifiedTopology: true, useNewUrlParser: true}) //promise = un objet
+  .then(() => app.listen(4000)) //promise.then => quand la promise est résolue, faire xxx
+  .catch(error => console.log(error)); //catch attrape les erreurs et permet de les traiter
